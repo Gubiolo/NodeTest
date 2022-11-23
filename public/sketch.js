448bd6978c909c28d8cc03c8c37181f232eaccca
Gubiolo <stefano.gubiolo@gmail.com>
2022-11-23 11:10:05 +0100

    push

---
+++ public/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+   background(220);
+}
+
+function draw() {
+ 
+
+  rect(mouseX, mouseY, 20, 20);
+}
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Ciao Ciao");
+console.log("Uomini Forti Destini Forti");
+
+
+//how upload the module to the client
+let express = require("express");
+//activeting express
+let app = express();
+
+//Define the port number
+let port = 3000;
+//ask to express to wait to info from port 3000
+let server = app.listen(port);
+
+console.log("running server on http://localhost:" + port);
+
+app.use(express.static("public"));
